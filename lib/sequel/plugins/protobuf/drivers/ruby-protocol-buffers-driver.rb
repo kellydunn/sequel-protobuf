9f53ea5815b02aceb950156251bd333a6b85a97d
kellydunn <defaultstring@gmail.com>
2014-01-12 17:47:01 -0800

    Adding some YARDdoc style documentation to the ruby-protocol-buffers driver

--- lib/sequel/plugins/protobuf/drivers/ruby-protocol-buffers-driver.rb
+++ lib/sequel/plugins/protobuf/drivers/ruby-protocol-buffers-driver.rb
@@ -2,12 +2,27 @@ module Sequel
   module Plugins
     module Protobuf
       module Drivers
+        
+        # This driver definition provides a standard interface
+        # for protocol buffer serialization with the `ruby-protocol-buffers` gem.
         module RubyProtocolBuffers
              
+          # Parses the passed in protobuf message into 
+          # an instance of the corresponding protocol buffer model definition
+          #
+          # @param protobuf_model {Object}.  The class of the protocol buffer definition
+          # @param protobuf {String}.  The protocol buffer message to be parsed
+          # @param options {Hash}.  An options hash to help configure the parsing functionality.
           def self.parse(protobuf_model, protobuf, options = {})
             return protobuf_model.send(:parse, protobuf)
           end
           
+          # Serializes the passed in attributes hash into an instance of the passed in
+          # protocol buffer model definition.
+          #
+          # @param protobuf_model {Object}. The class of the protocol buffer definition
+          # @param attributes {Hash}.  The attributes in which to be serialized into a protocol buffer message
+          # @param options {Hash}.  An options hash to help configure the serialization funcitonality.
           def self.serialize(protobuf_model, attributes, options = {})
             return protobuf_model.send(:new, attributes).to_s
           end
