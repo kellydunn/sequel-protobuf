a4103d6c7b51556dfa08748725858172611c809b
kellydunn <defaultstring@gmail.com>
2014-03-08 12:56:32 -0800

    Adding in new documentation for the older self.create method

--- lib/sequel/plugins/protobuf/drivers/ruby-protocol-buffers-driver.rb
+++ lib/sequel/plugins/protobuf/drivers/ruby-protocol-buffers-driver.rb
@@ -25,10 +25,19 @@ module Sequel
           # @param protobuf_model {Object}. The class of the protocol buffer definition
           # @param attributes {Hash}.  The attributes in which to be serialized into a protocol buffer message
           # @param options {Hash}.  An options hash to help configure the serialization funcitonality.
+          # @return {::ProtocolBuffers::Message}.  A newly created protocol buffer message
           def self.serialize(protobuf_model, attributes, options = {})
             return self.create(protobuf_model, attributes, options).to_s
           end
 
+          # Creates and returns a new instance of the specified protobuf model 
+          # initialized with the passed in attributes and configured by the passed in optoins.
+          # 
+          # @param protobuf_model {Object}.  The protobuf model in which to render.
+          # @param attributes {Hash}.  A hash of attributes in which to create the new protobuf model
+          # @param options {Hash}.  A configuration hash in which to configure the creation of the
+          #                         New protocol buffer object.
+          # @return {::ProtocolBuffers::Message}.  A newly created protocol buffer message.
           def self.create(protobuf_model, attributes, options = {})
             fields = protobuf_model.fields.inject([]) do |acc, (k, v)| 
               acc << v.name
